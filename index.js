c6ee3b13fdcca876ddba8d97228ad888cdf9798d
xudong <xdong-huang@haihangyun.com>
2017-11-27 09:30:16 +0800

    change to standard example

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 module.exports = {
+  // Map of hooks
   hooks: {
     // Index each page
     'page': function (page) {
       console.log('page ok');
     },
     // Write index to disk
-    'finish': function() {
+    'finish': function () {
 
     }
-  }
+  },
+  // Map of new blocks
+  blocks: {},
+
+  // Map of new filters
+  filters: {}
 };
